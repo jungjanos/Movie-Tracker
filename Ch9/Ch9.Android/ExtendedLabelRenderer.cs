5db882d37aeb0382fcd7c0aaf0688ec84d49e074
Jung Janos <janos.jung@outlook.com>
2019-04-04 20:07:36 +0200

    Added a custom LabelRenderer from StackOverflow: https://stackoverflow.com/questions/30062368/xamarin-forms-label-justify/53945760

---
+++ Ch9/Ch9.Android/ExtendedLabelRenderer.cs
@@ -0,0 +1,29 @@
+﻿using Android.Content;
+using Ch9;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(ExtendedLabel), typeof(Ch9.Droid.ExtendedLabelRenderer))]
+namespace Ch9.Droid
+{
+    public class ExtendedLabelRenderer : LabelRenderer
+    {
+        public ExtendedLabelRenderer(Context context) : base(context)
+        {}
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            var el = (Element as ExtendedLabel);
+
+            if (el != null && el.JustifyText)
+            {
+                if (Android.OS.Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.O)
+                {
+                    Control.JustificationMode = Android.Text.JustificationMode.InterWord;
+                }
+            }
+        }
+    }
+}

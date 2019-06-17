45bbd7891ab5c090d89405f55401f06117710c97
Jung Janos <janos.jung@outlook.com>
2019-06-17 15:04:31 +0200

    -Added Star glyphs as rating buttons to ReviewsPage -Using Segoe MDL2 glyphs

--- Ch9/Ch9/ReviewsPage.xaml.cs
+++ Ch9/Ch9/ReviewsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,4 +25,43 @@ namespace Ch9
             InitializeComponent();
         }
     }
+
+    public class RatingToGlyphConverter : IValueConverter
+    {
+        private readonly string _favoriteStar = "\ue734";
+        private readonly string _favoriteStarFill = "\ue735";
+        private readonly string _outlineQuarterStarLeft = "\uf0e5";
+        private readonly string _outlineHalfStarLeft = "\uf0e7";
+        private readonly string _outlineThreeQuarterStarLeft = "\uf0e9";
+
+        public object Convert(object value, Type targetType, object glyphPosistion, CultureInfo culture)
+        {
+            int position = int.Parse((string)glyphPosistion);
+
+            decimal? rating = value as decimal?;
+
+            if (rating == null)
+                return _favoriteStar;
+
+            decimal starValue = rating.Value / 2;
+
+            if (0 <= starValue - (position + 1))
+                return _favoriteStarFill;
+
+            if (0.75M <= starValue - (position))
+                return _outlineThreeQuarterStarLeft;
+
+            if (0.5M <= starValue - (position))
+                return _outlineHalfStarLeft;
+
+            if (0.25M <= starValue - (position))
+                return _outlineQuarterStarLeft;
+
+            return _favoriteStar;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        { throw new NotImplementedException(); }
+    }
+
 }
--- Ch9/Ch9/ReviewsPageViewModel.cs
+++ Ch9/Ch9/ReviewsPageViewModel.cs
@@ -2,13 +2,18 @@
 using Ch9.Models;
 using Ch9.Utils;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace Ch9
 {
-    public class ReviewsPageViewModel
+    public class ReviewsPageViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         private readonly MovieDetailPageViewModel _parent;
         private readonly ITmdbCachedSearchClient _cachedSearchClient;
         private readonly Task _initializer;
@@ -25,6 +30,25 @@ namespace Ch9
             _initializer = Initialize();
         }
 
+        // This stupidity is required bc of the way the Server 
+        // side wraps the rating into a variably type Json object.
+        public decimal? UsersRating 
+        {
+            get => _parent.MovieStates.Rating?.Value;
+            set
+            {
+                if (value == UsersRating)
+                    return;
+
+                if (value == null)
+                    _parent.MovieStates.Rating = null;
+                else
+                    _parent.MovieStates.Rating.Value = value.Value;
+
+                OnPropertyChanged(nameof(UsersRating));
+            }
+        }
+
         private async Task Initialize()
         {
             var getReviewResult = await _cachedSearchClient.GetMovieReviews(Movie.Id, language: null, page: null, retryCount: 3, delayMilliseconds: 1000, fromCache: false);
@@ -36,6 +60,19 @@ namespace Ch9
                     Reviews.Add(review);
                 _parent.MovieHasReviews = Reviews.Count > 0;
             }
+        }
+        private void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+                return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
         }
     }
 }

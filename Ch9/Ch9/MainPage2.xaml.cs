4e9632b04b33f3d8ef9552ee0d9ceb3295bfc504
Jung Janos <janos.jung@outlook.com>
2019-06-23 13:05:35 +0200

    Refactoring MainPage to MVVM: -0, Added MainPage2 and MainPage2ViewModel as V and VM

---
+++ Ch9/Ch9/MainPage2.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ch9
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage2 : ContentPage
+    {
+        public MainPage2ViewModel ViewModel
+        {
+            get => BindingContext as MainPage2ViewModel;
+            set => BindingContext = value;
+        }
+
+        public MainPage2()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Ch9/Ch9/MainPage2ViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Ch9.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Ch9
+{
+    public class MainPage2ViewModel : INotifyPropertyChanged
+    {        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _searchString;
+        public string SearchString
+        {
+            get => _searchString;
+            set => SetProperty(ref _searchString, value);
+        }
+
+        private MovieDetailModel _searchResults;
+        public MovieDetailModel SearchResults
+        {
+            get => _searchResults;
+            set => SetProperty(ref _searchResults, value);
+        }
+
+
+
+        private void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+                return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}

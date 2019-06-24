b592934557e31ad6390498c56d021344bf3680fe
Jung Janos <janos.jung@outlook.com>
2019-06-24 14:51:23 +0200

    Refactoring MainPage to MVVM: -2, implemented search

--- Ch9/Ch9/MainPage2.xaml.cs
+++ Ch9/Ch9/MainPage2.xaml.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Ch9.Utils;
+using System;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -22,22 +18,30 @@ namespace Ch9
         public MainPage2()
         {
             InitializeComponent();
+            ViewModel = new MainPage2ViewModel(
+                ((App)Application.Current).Settings,
+                ((App)Application.Current).CachedSearchClient,
+                ((App)Application.Current).MovieDetailModelConfigurator,
+                new PageService(this));
         }
     }
 
-    // returns whether or not the text length is smaller than the integer parameter
-    public class TextLengthIsGreaterThanIntegerValueToBoolConverter : IValueConverter
+    // returns whether or not the search text info (e.g. type at least # characters) should be displayed
+    // search test info should be displayed if the user has already typed some characters, 
+    // but not enought to initiate a search
+    // the auxilary parameter is expected as integer and denotes the minimum allowed search length
+    public class SearchStringToIsVisibleBoolConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             string txt = value as string;
-            if (txt == null)
-                return true;
-            
+            if (string.IsNullOrEmpty(txt))
+                return false;
+
             return txt.Length < (int)parameter;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {throw new NotImplementedException();}
+        { throw new NotImplementedException(); }
     }
 }
--- Ch9/Ch9/MainPage2ViewModel.cs
+++ Ch9/Ch9/MainPage2ViewModel.cs
@@ -1,10 +1,12 @@
 ﻿using Ch9.ApiClient;
 using Ch9.Models;
 using Ch9.Utils;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,7 +22,7 @@ namespace Ch9
         private readonly ISettings _settings;
         private readonly ITmdbCachedSearchClient _cachedSearchClient;
         private readonly IMovieDetailModelConfigurator _movieDetailModelConfigurator;
-
+        private readonly IPageService _pageService;
         private ObservableCollection<MovieDetailModel> _searchResults;
         public ObservableCollection<MovieDetailModel> SearchResults
         {
@@ -32,29 +34,55 @@ namespace Ch9
         public string SearchString
         {
             get => _searchString;
-            set => SetProperty(ref _searchString, value);
+            set
+            {
+                if (SetProperty(ref _searchString, value))
+                {
+                    if (string.IsNullOrEmpty(SearchString))
+                        SearchResults.Clear();
+                }
+            } 
         }
-
-        public int MinimumSearchStringLenth => MinimumSearchStringLenth;
 
         public ICommand SearchCommand { get; private set; }
 
         public MainPage2ViewModel(
             ISettings settings, 
             ITmdbCachedSearchClient cachedSearchClient,
-            IMovieDetailModelConfigurator movieDetailModelConfigurator)
+            IMovieDetailModelConfigurator movieDetailModelConfigurator,
+            IPageService pageService)
         {
             _settings = settings;
             _cachedSearchClient = cachedSearchClient;
             _movieDetailModelConfigurator = movieDetailModelConfigurator;
-
+            _pageService = pageService;
             SearchCommand = new Command(async () => await OnSearchCommand());
 
         }
 
         private async Task OnSearchCommand()
         {
-            return;
+            var searchResult = await _cachedSearchClient.SearchByMovie(SearchString, _settings.SearchLanguage, _settings.IncludeAdult);
+
+            if (searchResult.HttpStatusCode.IsSuccessCode())
+            {
+                try
+                {
+                    var deserializedApiResponse = JsonConvert.DeserializeObject<SearchResult>(searchResult.Json);
+                    var filteredResult = ((App)Application.Current).ResultFilter.FilterBySearchSettings(deserializedApiResponse.MovieDetailModels).ToList();
+
+                    _movieDetailModelConfigurator.SetImageSrc(filteredResult);
+                    _movieDetailModelConfigurator.SetGenreNamesFromGenreIds(filteredResult);
+
+                    SearchResults = new ObservableCollection<MovieDetailModel>(filteredResult);
+                }
+                catch (Exception ex)
+                {
+                    await _pageService.DisplayAlert("Error", $"The following exception was thrown: {ex.Message}", "Ok");
+                }
+            }
+            else
+                await _pageService.DisplayAlert("Network error", $"Server responded with the following error code: {searchResult.HttpStatusCode}", "Ok");
         }
 
         public event PropertyChangedEventHandler PropertyChanged;

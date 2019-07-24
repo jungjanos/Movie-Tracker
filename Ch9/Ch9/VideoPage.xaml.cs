2fbd983226e45ee218f4296b58f7bb10d36e625d
Jung Janos <janos.jung@outlook.com>
2019-07-24 16:37:48 +0200

    -Added code for Video Player from : https://docs.microsoft.com/en-us/xamarin/xamarin-forms/app-fundamentals/custom-renderer/video-player/web-videos -Added stub VideoPage

---
+++ Ch9/Ch9/VideoPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ch9
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VideoPage : ContentPage
+    {
+        public MovieDetailPageViewModel ViewModel
+        {
+            get => BindingContext as MovieDetailPageViewModel;
+            set => BindingContext = value;
+        }
+
+        public VideoPage(MovieDetailPageViewModel viewModel)
+        {
+            ViewModel = viewModel;
+            InitializeComponent();
+        }
+    }
+}

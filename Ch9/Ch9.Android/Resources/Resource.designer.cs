5554586d511f8cfe2eb09d2927929972367e5ef0
Jung Janos <janos.jung@outlook.com>
2019-08-06 18:12:40 +0200

    Added FFImageLoading to the solution: -(1) Added package and reference to the project -(2) Replaced Image in XAML to CachedImage when loading from Url -(3) Added png images as error placeholders (Resources folder) -(4) Minor changes

--- Ch9/Ch9.Android/Resources/Resource.designer.cs
+++ Ch9/Ch9.Android/Resources/Resource.designer.cs
@@ -8470,26 +8470,26 @@ namespace Ch9.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
+			// aapt resource value: 0x7f02013c
+			public const int avd_hide_password_1 = 2130837820;
+			
+			// aapt resource value: 0x7f02013d
+			public const int avd_hide_password_2 = 2130837821;
+			
 			// aapt resource value: 0x7f02013e
-			public const int avd_hide_password_1 = 2130837822;
-			
-			// aapt resource value: 0x7f02013f
-			public const int avd_hide_password_2 = 2130837823;
-			
-			// aapt resource value: 0x7f020140
-			public const int avd_hide_password_3 = 2130837824;
+			public const int avd_hide_password_3 = 2130837822;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
+			// aapt resource value: 0x7f02013f
+			public const int avd_show_password_1 = 2130837823;
+			
+			// aapt resource value: 0x7f020140
+			public const int avd_show_password_2 = 2130837824;
+			
 			// aapt resource value: 0x7f020141
-			public const int avd_show_password_1 = 2130837825;
-			
-			// aapt resource value: 0x7f020142
-			public const int avd_show_password_2 = 2130837826;
-			
-			// aapt resource value: 0x7f020143
-			public const int avd_show_password_3 = 2130837827;
+			public const int avd_show_password_3 = 2130837825;
 			
 			// aapt resource value: 0x7f020056
 			public const int design_bottom_navigation_item_background = 2130837590;
@@ -9148,11 +9148,11 @@ namespace Ch9.Droid
 			// aapt resource value: 0x7f020130
 			public const int notification_icon_background = 2130837808;
 			
-			// aapt resource value: 0x7f02013c
-			public const int notification_template_icon_bg = 2130837820;
+			// aapt resource value: 0x7f02013a
+			public const int notification_template_icon_bg = 2130837818;
 			
-			// aapt resource value: 0x7f02013d
-			public const int notification_template_icon_low_bg = 2130837821;
+			// aapt resource value: 0x7f02013b
+			public const int notification_template_icon_low_bg = 2130837819;
 			
 			// aapt resource value: 0x7f020131
 			public const int notification_tile_bg = 2130837809;
@@ -9170,22 +9170,16 @@ namespace Ch9.Droid
 			public const int Poster_sm_placeholder = 2130837813;
 			
 			// aapt resource value: 0x7f020136
-			public const int profile_image_placeholder_lg = 2130837814;
+			public const int settings = 2130837814;
 			
 			// aapt resource value: 0x7f020137
-			public const int profile_image_placeholder_sm = 2130837815;
+			public const int settings2 = 2130837815;
 			
 			// aapt resource value: 0x7f020138
-			public const int settings = 2130837816;
+			public const int tooltip_frame_dark = 2130837816;
 			
 			// aapt resource value: 0x7f020139
-			public const int settings2 = 2130837817;
-			
-			// aapt resource value: 0x7f02013a
-			public const int tooltip_frame_dark = 2130837818;
-			
-			// aapt resource value: 0x7f02013b
-			public const int tooltip_frame_light = 2130837819;
+			public const int tooltip_frame_light = 2130837817;
 			
 			static Drawable()
 			{

68977c6921f8e053819ec0e2812ebc4ae81a7534
Jung Janos <janos.jung@outlook.com>
2019-08-17 14:26:26 +0200

    LoginPage: -(1) Added first implementation of the LoginPage (View) and ViewModel skeleton

---
+++ Ch9/Ch9/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+
+namespace Ch9.ViewModels
+{
+    public class LoginPageViewModel : INotifyPropertyChanged
+    {
+        private string _userName;      
+        public string UserName
+        {
+            get => _userName;
+            set => SetProperty(ref _userName, value);
+        }
+
+        private string _password;       
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+
+        private bool _ignoreLoginFlag;
+        public bool IgnoreLoginFlag
+        {
+            get => _ignoreLoginFlag;
+            set => SetProperty(ref _ignoreLoginFlag, value);
+        }
+
+        public ICommand SubmitCommand { get; private set; } 
+        public ICommand SignUpCommand { get; private set; }
+        public ICommand CancelCommand { get; private set; }
+
+
+        public LoginPageViewModel()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName]string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+                return false;
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
---
+++ Ch9/Ch9/Views/LoginPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Ch9.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ch9.Views
+{
+    //[XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPageViewModel ViewModel
+        {
+            get => BindingContext as LoginPageViewModel;
+            set => BindingContext = value;
+        }
+
+        public LoginPage()
+        {
+            ViewModel = new LoginPageViewModel();
+            InitializeComponent();
+        }
+    }
+}

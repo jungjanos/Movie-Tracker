4366604c3eb27574309dbfd41a1afb62002ae834
Jung Janos <janos.jung@outlook.com>
2019-11-25 14:04:41 +0100

    copied relevant source files to the new project for the Data-layer

---
+++ Ch9.Data.Contracts/ApiClientResponseModels.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Net;
+
+namespace Ch9.Data.Contracts
+{
+    public class TmdbResponseBase
+    {
+        public HttpStatusCode HttpStatusCode { get; set; }
+        public string Json { get; set; }
+    }
+
+    // TmdbResponseBase's derived classes use the same result format,
+    // inheritacne is used only to give a more descriptive class names
+    // and provide information about the API call used to get the results
+
+    public class TmdbConfigurationModelResult : TmdbResponseBase
+    { }
+
+    public class GenreNameFetchResult : TmdbResponseBase
+    { }
+
+    public class SearchByMovieResult : TmdbResponseBase
+    { }
+
+    public class TrendingMoviesResult : TmdbResponseBase
+    { }
+
+    public class GetMovieDetailsResult : TmdbResponseBase
+    { }
+
+    public class GetMovieDetailsWithAccountStatesResult : TmdbResponseBase
+    {
+        public GetMovieDetailsWithAccountStatesResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class GetMovieImagesResult : TmdbResponseBase
+    { }
+
+    public class GetPersonImagesResult : TmdbResponseBase
+    { }
+
+    public class GetMovieVideosResult : TmdbResponseBase
+    { }
+
+    public class GetMovieRecommendationsResult : TmdbResponseBase
+    { }
+
+    public class GetSimilarMoviesResult : TmdbResponseBase
+    { }
+
+    public class CreateRequestTokenResult : TmdbResponseBase
+    { }
+
+    public class CreateSessionIdResult : TmdbResponseBase
+    { }
+
+    public class DeleteSessionResult : TmdbResponseBase
+    { }
+
+    public class GetAccountDetailsResult : TmdbResponseBase
+    { }
+
+    public class GetListsResult : TmdbResponseBase
+    { }
+
+    public class CreateListResult : TmdbResponseBase
+    { }
+
+    public class DeleteListResult : TmdbResponseBase
+    { }
+
+    public class GetListDetailsResult : TmdbResponseBase
+    { }
+
+    public class AddMovieResult : TmdbResponseBase
+    { }
+
+    public class RemoveMovieResult : TmdbResponseBase
+    { }
+
+    public class GetMovieReviewsResult : TmdbResponseBase
+    { }
+
+    public class GetMovieWatchlistResult : TmdbResponseBase
+    { }
+
+    public class GetFavoriteMoviesResult : TmdbResponseBase
+    { }
+
+    public class UpdateWatchlistResult : TmdbResponseBase
+    { }
+
+    public class UpdateFavoriteListResult : TmdbResponseBase
+    { }
+
+    public class RateMovieResult : TmdbResponseBase
+    { }
+
+    public class DeleteMovieRatingResult : TmdbResponseBase
+    { }
+
+    public class GetMovieCreditsResult : TmdbResponseBase
+    { }
+
+    public class GetPersonsMovieCreditsResult : TmdbResponseBase
+    { }
+
+    public class GetPersonsDetailsResult : TmdbResponseBase
+    { }
+
+    public class GetItemStatusOnTargetListResult : TmdbResponseBase
+    { }
+
+    /// <summary>
+    /// Because of the custom deserialization requirement (the WebAPI sends back flexible object type as response)
+    /// The deserialization is built into the response object itself. This behavior diverges from normal case
+    /// </summary>
+    public class GetAccountMovieStatesResult : TmdbResponseBase
+    {
+        /// <summary>
+        /// Can trow. 
+        /// If the HttpStatusCode property is 200(OK) then it tries to deserialize the Json response into 
+        /// the model.
+        /// </summary>
+        /// <returns>object containing the account's states for a particular movie (rating, on watchlit, on favorite list) </returns>
+
+        public GetAccountMovieStatesResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Ch9.Data.Contracts/Rating.cs
@@ -0,0 +1,29 @@
+﻿namespace Ch9.Data.Contracts
+{
+    //TODO : would be nice to eliminate this stand alone class by serializing its 
+    // UI counterpart to JSON and feeding the JSON into the ApiClient via string
+    // no value in this data structure
+    public enum Rating
+    {
+        Half = 1,
+        One = 2,
+        OneAndHalf = 3,
+        Two = 4,
+        TwoAndHalf = 5,
+        Three = 6,
+        ThreeAndHalf = 7,
+        Four = 8,
+        FourAndHalf = 9,
+        Five = 10,
+        FiveAndHalf = 11,
+        Six = 12,
+        SixAndHalf = 13,
+        Seven = 14,
+        SevenAndHalf = 15,
+        Eight = 16,
+        EightAndHalf = 17,
+        Nine = 18,
+        NineAndHalf = 19,
+        Ten = 20
+    }
+}

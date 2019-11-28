641775822701fd281215e5b6dd293811463683a6
Jung Janos <janos.jung@outlook.com>
2019-11-28 16:59:49 +0100

    ConfigurationCache is now moved into the Services assembly

---
+++ Ch9.Services.Contracts/ITmdbConfigurationCache.cs
@@ -0,0 +1,13 @@
+﻿using Ch9.Ui.Contracts.Models;
+using System.Threading.Tasks;
+
+namespace Ch9.Services.Contracts
+{
+    // tries to get the TMDB configuration from the server, caches the result in the property dictionary
+    // if contacting the server fails, it consults the property dictionary
+    // if the property dictionary contains no entry, the static defaults are returned 
+    public interface ITmdbConfigurationCache
+    {
+        Task<TmdbConfigurationModel> FetchTmdbConfiguration(int retries = 0, int retryDelay = 1000);
+    }
+}
--- Ch9.Services/LocalSettings/TmdbConfigurationCache.cs
+++ Ch9.Services/LocalSettings/TmdbConfigurationCache.cs
@@ -1,5 +1,5 @@
 ﻿using Ch9.Ui.Contracts.Models;
-using Ch9.Utils;
+using Ch9.Infrastructure.Extensions;
 using Ch9.Services.Contracts;
 using Ch9.Data.Contracts;
 using Newtonsoft.Json;
@@ -8,14 +8,6 @@ using System.Threading.Tasks;
 
 namespace Ch9.Services
 {
-    // tries to get the TMDB configuration from the server, caches the result in the property dictionary
-    // if contacting the server fails, it consults the property dictionary
-    // if the property dictionary contains no entry, the static defaults are returned 
-    public interface ITmdbConfigurationCache
-    {
-        Task<TmdbConfigurationModel> FetchTmdbConfiguration(int retries = 0, int retryDelay = 1000);
-    }
-
     public class TmdbConfigurationCache : ITmdbConfigurationCache
     {        
         private readonly ITmdbApiService _tmdbApiService;
